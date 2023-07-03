067db4589f0c2defcff1e170b5802b976e1bc006
AbhayGarg02 <94742089+AbhayGarg02@users.noreply.github.com>
2023-07-03 20:57:37 +0530

    Update a portfolio.js

--- a portfolio.js
+++ a portfolio.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web desinger" , "Content Creater"],
+        strings: ["Web Developer" , "Content Creator"],
         typeSpeed: 100,
         backSpeed: 80,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web desinger", "Content Creater"],
+        strings: ["Web Developer", "Content Creator"],
         typeSpeed: 100,
         backSpeed: 80,
         loop: true
